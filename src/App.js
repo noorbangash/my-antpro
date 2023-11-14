e17802aea13acd3d6a01259bb51c95e6b2c1dac2
M.Fareed <fareed@gmail.com>
2023-11-14 15:51:17 +0500

    add new file noor.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Prac from './Prac';
+
+function App() {
+  return (
+    <div className="App">
+    <Prac/>
+    </div>
+  );
+}
+
+export default App;
