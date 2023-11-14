d336af96b4d54bead226138de2e7c2e73e129193
M.Fareed <fareed@gmail.com>
2023-11-14 16:34:49 +0500

    add Noor.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Prac from './Prac';
+import Noor from './Noor';
 
 function App() {
   return (
     <div className="App">
     <Prac/>
+     <Noor/>
     </div>
   );
 }
